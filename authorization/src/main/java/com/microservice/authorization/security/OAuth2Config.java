c5b7e85e41fdea102a90766382cba1f3787c32db
nubnasir <nubnasir@gmail.com>
2021-02-21 23:40:55 +0600

    Cleaning comments after implementation

--- authorization/src/main/java/com/microservice/authorization/security/OAuth2Config.java
+++ authorization/src/main/java/com/microservice/authorization/security/OAuth2Config.java
@@ -11,61 +11,6 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Aut
 
 import javax.sql.DataSource;
 
-/*-- Tables for OAuth token store
-
-CREATE TABLE oauth_client_details (
-client_id               VARCHAR(255) PRIMARY KEY,
-resource_ids            VARCHAR(255),
-client_secret           VARCHAR(255),
-scope                   VARCHAR(255),
-authorized_grant_types  VARCHAR(255),
-web_server_redirect_uri VARCHAR(255),
-authorities             VARCHAR(255),
-access_token_validity   INTEGER,
-refresh_token_validity  INTEGER,
-additional_information  VARCHAR(4096),
-autoapprove             TINYINT
-);
-
-CREATE TABLE oauth_client_token (
-token_id          VARCHAR(255),
-token             BLOB,
-authentication_id VARCHAR(255),
-user_name         VARCHAR(255),
-client_id         VARCHAR(255)
-);
-
-CREATE TABLE oauth_access_token (
-token_id          VARCHAR(255),
-token             BLOB,
-authentication_id VARCHAR(255),
-user_name         VARCHAR(255),
-client_id         VARCHAR(255),
-authentication    BLOB,
-refresh_token     VARCHAR(255)
-);
-
-CREATE TABLE oauth_refresh_token (
-token_id       VARCHAR(255),
-token          BLOB,
-authentication BLOB
-);
-
-CREATE TABLE oauth_code (
-code           VARCHAR(255),
-authentication BLOB
-);
-
-
-Implementation
-clients.jdbc(dataSource).withClient("clientapp")
-           .authorizedGrantTypes("password", "refresh_token")
-           .authorities("USER")
-           .scopes("read", "write")
-           .resourceIds(RESOURCE_ID)
-           .secret("123456").and().build();
-*/
-
 @Configuration
 public class OAuth2Config  extends AuthorizationServerConfigurerAdapter {
 
